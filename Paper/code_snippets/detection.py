f015b91dafc4ab055a5e3e92d0fbe27d75867cb7
nToTheG <nelio.gautschi@icloud.com>
2025-10-12 17:39:33 +0200

    12.10.25 - Kapitel 3.2 fertig

---
+++ Paper/code_snippets/detection.py
@@ -0,0 +1,33 @@
+"Autor: Nelio Gautschi"
+
+import cv2 # Importiert die OpenCV-Bibliothek für Bildverarbeitung
+
+cam = cv2.VideoCapture(0) # Zugriff auf die Kamera
+
+# Überprüfen, ob die Kamera erfolgreich geöffnet wurde
+if not cam.isOpened():
+    raise IOError("Cannot open camera.")
+
+while True: # Schleife zum kontinuierlichen Einlesen von Bildern
+    success, frame = cam.read()
+    if not success:
+        print("Cannot receive frame. Exiting ...")
+        break
+
+    # Umwandeln des Farbbilds in ein Graustufenbild
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Spiegelung entlang der vertikalen Achse
+    flipped_frame = cv2.flip(gray_frame, 1)
+
+    # Definition der verwendeten ArUco-Bibliothek
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+
+    # Erstellen eines Parameterobjekts für den Erkennungsprozess
+    parameters = cv2.aruco.DetectorParameters()
+
+    # Initialisieren des ArUco-Detektors
+    detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+
+    # Erkennen der Marker im aktuellen Kamerabild
+    corners, ids, rejected = detector.detectMarkers(flipped_frame)
